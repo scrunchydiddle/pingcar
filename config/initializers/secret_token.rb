d11696668e6fc7e30089a2ca4804c1041c36c601
ikky <ikky@ikky-desktop.(none)>
2011-10-15 13:08:34 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pingcar::Application.config.secret_token = '99b33114522b48fdac77cf69d4de8098e14127bf6b8a4f906f75a3108d40156a17bb9c289799c875b65f9332e0f793aab128c17ad5a59d50b0022adc0872539e'
